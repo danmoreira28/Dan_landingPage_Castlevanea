151caedf460c9ffaf13ceac59ddd516b18985c8c
Danilo Moreira <danilo_moreirasilva@hotmail.com>
2024-04-03 16:22:38 -0300

    Commit suporte

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin')
 
 function styles(){
-    return gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/main.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
@@ -14,7 +14,7 @@ function images(){
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default =gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
